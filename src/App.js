56b428abb1a21efc48ebe5a158a83ed6cc3817e2
David Kinnear <66265062+davidkinnear@users.noreply.github.com>
2020-09-08 16:18:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Nav from './Nav';
 import requests from './requests'
 
 function App() {
-  
+   //selctedOption allows the results to change with what nav title is selected
   const [selectedOption, setSelectedOption] = useState(requests.fetchTrending)
   return (
     <div className="app">
