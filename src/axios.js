4c40a0a3c5282143777806454f46cd5f57696e72
David Kinnear <66265062+davidkinnear@users.noreply.github.com>
2020-09-08 16:18:33 -0500

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,8 @@
+//axios is library to make requests to the end point of API
 import axios from 'axios';
 
+//whenever I make request I don't have to include beginning of URL
+//appends this to the beginning of the requests.js URLs
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
 })
