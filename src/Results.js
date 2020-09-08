1819b226cf4143a8efce0345247a8f92566b35e8
David Kinnear <66265062+davidkinnear@users.noreply.github.com>
2020-09-08 16:20:11 -0500

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -5,22 +5,27 @@ import axios from './axios'
 import FlipMove from 'react-flip-move'
 
 function Results({ selectedOption }) {
+//state is like writing a variable in React
     const [movies, setMovies] = useState(["1", "2", "3"]);
-
     useEffect(() => {
         // run this code once when the Results component loads/mount
         async function fetchData() {
+        //await means waiting for response to come back
+        //returns an array of all the movies from the selectedOption
             const request = await axios.get(selectedOption);
             console.log(request.data.results);
             setMovies(request.data.results);
             return request;
         }
         fetchData();
-    }, [selectedOption])
+    }, [selectedOption]) //runs code every time selectedOption changes
+    //flipmove is a react library to add a nice animation when switching to different nav titles
     return (
         <div className="results">
-            <FlipMove>
+            <FlipMove> 
                 {movies.map((movie) => (
+     //mapping through the array of movies and passing the movie as a prop into the VideoCard  
+     //key allows React to not have to re-render the entire list, just the VideoCard             
                 <VideoCard key={movie.id} movie={movie} />
                 ))}
             </FlipMove>
